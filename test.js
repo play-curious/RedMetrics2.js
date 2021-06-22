7630c5a834d886e8a4033706e830c5a42d171389
CamilleAbella <camille.abella@hotmail.fr>
2021-06-22 16:13:23 +0200

    try to fix tests

--- test.js
+++ test.js
@@ -1,42 +1,43 @@
-require("dotenv").config()
+require("dotenv/config");
 
 describe("cjs", () => {
-    const RedMetrics = require(".")
+  const RedMetrics = require(".");
 
-    const client = new RedMetrics.Client({
-        bufferingDelay: 1000,
-        apiKey: process.env.API_KEY,
-        apiConfig: RedMetrics.defaultDevConfig,
-        gameSession: {
-            gameVersionId: "a799a785-4841-402e-b484-e25479a6b2e4"
-        }
-    })
+  const client = new RedMetrics.Client({
+    bufferingDelay: 1000,
+    apiKey: process.env.API_KEY,
+    apiConfig: RedMetrics.defaultDevConfig,
+    gameSession: {
+      gameVersionId: "7fe48815-5d14-4e35-9c49-adc5d782c9ae",
+    },
+  });
 
-    test("wait 2s", (cb) => {
-        setTimeout(() => cb(), 2000)
-    })
+  test("wait 2s", (cb) => {
+    setTimeout(() => cb(), 2000);
+  });
 
-    test("is connected", () => {
-        expect(client.isConnected).toBeTruthy()
-    })
+  test("is connected", () => {
+    expect(client.isConnected).toBeTruthy();
+  });
 
-    test("send events",() => {
-        client.emit("start", {
-            custom_data: {
-                content: "Hello World!"
-            }
-        })
+  test("send events", () => {
+    client.emit("start", {
+      custom_data: {
+        content: "Hello World!",
+      },
+    });
 
-        client.emit("end", {
-            custom_data: {
-                content: "Another event!"
-            }
-        })
-    })
+    client.emit("end", {
+      custom_data: {
+        content: "Another event!",
+      },
+    });
+  });
 
-    test("disconnect", (cb) => {
-        client.disconnect()
-            .then(() => cb)
-            .catch(cb)
-    })
-})
+  test("disconnect", (cb) => {
+    client
+      .disconnect()
+      .then(() => cb)
+      .catch(cb);
+  });
+});
