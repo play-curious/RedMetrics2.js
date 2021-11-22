7a6ffec962c3e21125e68001a40cb4a5111665cf
CamilleAbella <camille.abella@hotmail.fr>
2021-11-22 11:26:22 +0100

    update module exports

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -6,10 +6,99 @@ var __createBinding = (this && this.__createBinding) || (Object.create ? (functi
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
 }));
-var __exportStar = (this && this.__exportStar) || function(m, exports) {
-    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-__exportStar(require("./app/client"), exports);
-const Client = require("./app/client");
-module.exports.Client = Client;
+const axios = __importStar(require("axios"));
+// todo start game session if gamesessionid not exists
+class Client {
+    constructor(config) {
+        this.config = config;
+        this.eventQueue = [];
+        this.bufferingInterval = null;
+        this.connected = false;
+        this.api = axios.default.create({
+            params: { apikey: config.apiKey },
+            baseURL: config.baseUrl,
+            headers: { "Access-Control-Allow-Origin": config.baseUrl },
+        });
+    }
+    get isConnected() {
+        return this.connected;
+    }
+    async connect() {
+        if (this.connected)
+            throw new Error("RedMetrics client is already connected");
+        const route = `/key`;
+        const { data: apiKey } = await this.api.get(route);
+        if (!apiKey) {
+            const session = this.config.session
+                ? {
+                    version: this.config.session.gameVersion,
+                    screen_size: this.config.session.screenSize,
+                    software: this.config.session.software,
+                    external_id: this.config.session.externalId,
+                    platform: this.config.session.platform,
+                    custom_data: this.config.session.customData === undefined
+                        ? undefined
+                        : JSON.stringify(this.config.session.customData),
+                }
+                : {};
+            const sessionRoute = `/session`;
+            const { data } = await this.api.post(sessionRoute, session);
+            this.sessionId = data.id;
+        }
+        else
+            this.sessionId = apiKey.key;
+        this.connected = true;
+        this.bufferingInterval = setInterval(this.buff.bind(this), this.config.bufferingDelay ?? 60000);
+    }
+    async disconnect(emitted) {
+        if (!this.connected)
+            throw new Error("RedMetrics client is not connected");
+        clearInterval(this.bufferingInterval);
+        this.emit("end", { ...emitted });
+        await this.buff();
+        this.bufferingInterval = null;
+        this.connected = false;
+    }
+    /**
+     * If you want to send events manually
+     */
+    async buff() {
+        if (this.connected && this.eventQueue.length > 0) {
+            const events = this.eventQueue.map((event) => ({
+                ...event,
+                session_id: this.sessionId,
+            }));
+            const eventRoute = "/event";
+            await this.api.post(eventRoute, events).then((res) => {
+                if (res.status == 200)
+                    this.eventQueue = [];
+            });
+            return true;
+        }
+        else if (!this.connected)
+            console.error("❌ redmetrics client not connected");
+        return false;
+    }
+    emit(type, event) {
+        this.eventQueue.push({
+            ...event,
+            type,
+            user_time: new Date().toISOString(),
+        });
+    }
+}
+exports.default = Client;
+module.exports = Client;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,138 @@
-export * from "./app/client";
+import * as axios from "axios";
+import * as types from "rm2-typings";
 
-const Client = require("./app/client");
+export interface SessionInfo {
+  gameId?: string;
+  gameVersion?: string;
+  customData?: object;
+  externalId?: string;
+  platform?: string;
+  screenSize?: string;
+  software?: string;
+}
 
-module.exports.Client = Client;
+export interface ClientConfig {
+  apiKey: string;
+  baseUrl: string;
+  session?: SessionInfo;
+  bufferingDelay?: number;
+}
+
+export type EmittedEvent = Omit<
+  types.tables.Event,
+  "user_time" | "type" | "server_time" | "id" | "session_id"
+>;
+
+// todo start game session if gamesessionid not exists
+
+export default class Client {
+  protected eventQueue: Omit<
+    types.tables.Event,
+    "server_time" | "session_id" | "id"
+  >[] = [];
+  protected bufferingInterval: any = null;
+  protected sessionId?: string;
+  protected connected = false;
+  protected api: axios.AxiosInstance;
+
+  constructor(public readonly config: ClientConfig) {
+    this.api = axios.default.create({
+      params: { apikey: config.apiKey },
+      baseURL: config.baseUrl,
+      headers: { "Access-Control-Allow-Origin": config.baseUrl },
+    });
+  }
+
+  get isConnected(): boolean {
+    return this.connected;
+  }
+
+  public async connect(): Promise<void> {
+    if (this.connected)
+      throw new Error("RedMetrics client is already connected");
+
+    const route: types.api.Key["Route"] = `/key`;
+    const { data: apiKey } = await this.api.get<
+      types.api.Key["Get"]["Response"]
+    >(route);
+
+    if (!apiKey) {
+      const session: types.api.Session["Post"]["Body"] = this.config.session
+        ? {
+            version: this.config.session.gameVersion,
+            screen_size: this.config.session.screenSize,
+            software: this.config.session.software,
+            external_id: this.config.session.externalId,
+            platform: this.config.session.platform,
+            custom_data:
+              this.config.session.customData === undefined
+                ? undefined
+                : JSON.stringify(this.config.session.customData),
+          }
+        : {};
+
+      const sessionRoute: types.api.Session["Route"] = `/session`;
+
+      const { data } = await this.api.post<
+        types.api.Session["Post"]["Response"]
+      >(sessionRoute, session);
+
+      this.sessionId = data.id;
+    } else this.sessionId = apiKey.key;
+
+    this.connected = true;
+
+    this.bufferingInterval = setInterval(
+      this.buff.bind(this),
+      this.config.bufferingDelay ?? 60000
+    );
+  }
+
+  public async disconnect(emitted?: EmittedEvent): Promise<void> {
+    if (!this.connected) throw new Error("RedMetrics client is not connected");
+
+    clearInterval(this.bufferingInterval);
+
+    this.emit("end", { ...emitted });
+
+    await this.buff();
+
+    this.bufferingInterval = null;
+    this.connected = false;
+  }
+
+  /**
+   * If you want to send events manually
+   */
+  async buff(): Promise<boolean> {
+    if (this.connected && this.eventQueue.length > 0) {
+      const events: types.api.Event["Post"]["Body"] = this.eventQueue.map(
+        (event) => ({
+          ...event,
+          session_id: this.sessionId as string,
+        })
+      );
+
+      const eventRoute: types.api.Event["Route"] = "/event";
+
+      await this.api.post(eventRoute, events).then((res) => {
+        if (res.status == 200) this.eventQueue = [];
+      });
+
+      return true;
+    } else if (!this.connected)
+      console.error("❌ redmetrics client not connected");
+
+    return false;
+  }
+
+  public emit(type: types.EventType, event: EmittedEvent) {
+    this.eventQueue.push({
+      ...event,
+      type,
+      user_time: new Date().toISOString(),
+    });
+  }
+}
+
+module.exports = Client;
